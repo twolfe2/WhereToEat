3845e61b7ce3e9b80735f57df6b2330c3f0c1a32
Thomas Wolfe <tewolfe2@gmail.com>
2016-04-15 19:07:48 -0500

    working

---
+++ static/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+    $('#new-city').hide();
+
+    $('form').on('submit', function() {
+
+        userInput = $('input[name="city"]').val();
+        console.log(userInput)
+
+        $.ajax({
+            type: "POST",
+            url: "/",
+            data: {'city': userInput},
+            success: function(results) {
+
+                if(results) {
+                    $('#submit').hide();
+                    $('input').hide();
+                    $('#new-city').show();
+                    console.log(results)
+                    console.log(results.name)
+                    $('#results').html('<a href="'+results.url+'">'+results.name+
+              '</a><br><img src="'+results.image+'" >')
+                } else {
+                    $('#results').html('Error, please try again.')
+                }
+            },
+            error: function(error) {
+                console.log(error)
+            }
+
+
+
+
+        });
+    });
+
+    $('#new-city').on('click', function(){
+        $('input').val('').show();
+        $('#new-city').hide();
+        $('#results').html('');
+        $('#submit').show();
+    });
+
+
+});
