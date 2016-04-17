22ef1a8b34418fdce2d88e88f33ff97f6138f934
Thomas Wolfe <tewolfe2@gmail.com>
2016-04-17 13:36:12 -0500

    front and back updated

--- static/main.js
+++ static/main.js
@@ -1,33 +1,32 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
 
 
     function show() {
-    $('#loading').hide();
-    $('#container').fadeIn();
-};
+
+        $('.container').fadeIn();
+    };
 
 
-setTimeout(show, 5000);
+    setTimeout(show, 50000);
 
-//
+    $('#results').hide();
+
 //var options = {
 //	data: ["blue", "green", "pink", "red", "yellow"]
 //};
 //
 //$("#basics").easyAutocomplete(options);
 //
-$('#new-city').hide();
+    $('#new-city').hide();
 
-$(function(){
-3
-$("#geocomplete").geocomplete()
-4
-});
+    $(function () {
+        3
+        $("#geocomplete").geocomplete()
+        4
+    });
 
 
-
-    $('form').on('submit', function() {
+    $('form').on('submit', function () {
 
         userInput = $('input[name="city"]').val();
         console.log(userInput)
@@ -36,34 +35,47 @@ $("#geocomplete").geocomplete()
             type: "POST",
             url: "/",
             data: {'city': userInput},
-            success: function(results) {
+            success: function (results) {
 
-                if(results) {
+                if (results) {
                     $('#submit').hide();
                     $('input').hide();
                     $('#new-city').show();
                     console.log(results)
                     console.log(results.name)
-                    $('#results').html('<a href="'+results.url+'">'+results.name+
-              '</a><br><img src="'+results.image+'" >')
+                    //$('#restInfo').text(results.info)
+                    $('#restName').text(results.name);
+                    $('#restImg').attr("src", results.image);
+                    $('#restPhone').hide();
+                    //don't show phone icon if no phone number
+                    if (results.phone !== "false") {
+                        $('#restPhone').attr("href", "tel:" + results.phone);
+                        $('#restPhone').show();
+                    }
+                    $('#restLink').attr("href", results.url);
+
+                    //      $('#results').html('<a href="'+results.url+'">'+results.name+
+                    //'</a><br><img src="'+results.image+'" >')
+                    $('#results').show();
+
                 } else {
+
                     $('#results').html('Error, please try again.')
+                    $('#results').show();
                 }
             },
-            error: function(error) {
+            error: function (error) {
                 console.log(error)
             }
-
-
 
 
         });
     });
 
-    $('#new-city').on('click', function(){
+    $('#new-city').on('click', function () {
         $('input').val('').show();
         $('#new-city').hide();
-        $('#results').html('');
+        $('#results').hide();
         $('#submit').show();
     });
 
